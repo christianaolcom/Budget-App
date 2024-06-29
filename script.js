e468077cfb136cd6568245ff8faf2827148dd16a
christianaolcom <christianaolaosebikan5@gmail.com>
2024-06-29 20:57:41 +0100

    initial

--- script.js
+++ script.js
@@ -1,43 +1,47 @@
-function getStarted(){
-    var money = owo.innerHTML
-    var amount = Quality.value + price.value
-    priceItem = Number(amount)
+let balance = 200000;
 
-    if (price.value == "" || product.value == "" || Quality.value =="") {
-        error.style.display = "block"
-        setTimeout(() => {
-            error.style.display = "none"
-        }, 2000);
-    }else if (priceItem > 0  && priceItem > money) {
-        errors.style.display = "block"
-        setTimeout(() => {
-            errors.style.display = "none"
-        }, 3000);
+    function getStarted() {
+        const product = document.getElementById('product').value;
+        const price = parseFloat(document.getElementById('price').value);
+        const quality = parseInt(document.getElementById('quality').value);
+        const totalCost = price * quality;
 
+        if (!product || !price || !quality) {
+            document.getElementById('error').style.display = "block";
+            setTimeout(() => {
+                document.getElementById('error').style.display = "none";
+            }, 2000);
+            return;
+        }
+
+        if (totalCost > balance) {
+            document.getElementById('errors').style.display = "block";
+            setTimeout(() => {
+                document.getElementById('errors').style.display = "none";
+            }, 3000);
+            return;
+        }
+
+        balance -= totalCost;
+        document.getElementById('owo').innerText = `# ${balance}`;
+
+        document.getElementById('show').innerHTML += `
+            <tr>
+                <td>${product}</td>
+                <td>${price}</td>
+                <td>${quality}</td>
+                <td>${totalCost}</td>
+            </tr>
+        `;
+
+        document.getElementById('product').value = "";
+        document.getElementById('price').value = "";
+        document.getElementById('quality').value = "";
+
+        if (balance <= 0) {
+            document.getElementById('errors').style.display = "block";
+            setTimeout(() => {
+                document.getElementById('errors').style.display = "none";
+            }, 3000);
+        }
     }
-
-    else{
-         
-        show.innerHTML += ` 
-        <tr>
-                    <td>${product.value}</td>
-                    <td>${price.value}</td>
-                    <td>${Quality.value}</td>
-                    <td>${price.value * Quality.value}</td>
-        
-
-                </tr>
-        `
-    }
-
-    document.getElementById('product').value =""
-    document.getElementById('price').value =""
-    document.getElementById('Quality').value = ""
-    document.getElementById('Amount'). value = ""
-    document.getElementById('Total'). value = ""
-
-}
-
-function login(){
-    location.href = "http://127.0.0.1:5500/index.html"
-}
